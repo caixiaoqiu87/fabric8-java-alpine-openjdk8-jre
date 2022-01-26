de96dbc4fb62fd9e978c304c1d76596ce53ee96c
xiaoqiu <caixiaoqiu87@foxmail.com>
2022-01-26 11:26:07 +0800

    Merge remote-tracking branch 'origin/aarch64' into aarch64

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t 113.209.134.184:11880/fczy/fabric8-java-alpine-openjdk8-jre:aarch64 .
-docker push 113.209.134.184:11880/fczy/fabric8-java-alpine-openjdk8-jre:aarch64
+docker build -t 113.209.134.184:11880/fczy/fabric8-java-alpine-openjdk8-jre:V3.15_aarch64 .
+docker push 113.209.134.184:11880/fczy/fabric8-java-alpine-openjdk8-jre:V3.15_aarch64
