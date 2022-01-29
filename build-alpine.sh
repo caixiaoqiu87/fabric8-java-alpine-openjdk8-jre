9e0bb3800c7e7c51c6dbc4dbcaa295d98ded8eb2
xiaoqiu <caixiaoqiu87@foxmail.com>
2022-01-29 10:56:40 +0800

    feature:v3.15.0

--- build-alpine.sh
+++ build-alpine.sh
@@ -1,2 +1,2 @@
-docker pull alpine:3.9
-docker tag alpine:3.9 113.209.134.184:11880/fczy/alpine:3.9_aarch64
+docker pull alpine:3.15.0
+docker tag alpine:3.15.0 113.209.134.184:11880/fczy/alpine:3.15.0_aarch64
