1e97b22be3720eb664a99597911cfdffbfd04ad7
xiaoqiu <caixiaoqiu87@foxmail.com>
2022-02-11 15:27:39 +0800

    feature:使用alpine3.15.0

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t 113.209.134.184:11880/fczy/fabric8-java-alpine-openjdk8-jre:aarch64_3.15.0 .
-docker push 113.209.134.184:11880/fczy/fabric8-java-alpine-openjdk8-jre:aarch64_3.15.0
+docker build -t 113.209.134.190:11880/fczy/fabric8-java-alpine-openjdk8-jre:aarch64_alpine3.15.0 .
+docker push 113.209.134.190:11880/fczy/fabric8-java-alpine-openjdk8-jre:aarch64_alpine3.15.0
